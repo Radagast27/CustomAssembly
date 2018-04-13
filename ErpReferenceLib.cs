65374d8f7cafce5c5770597703462c92e091638c
Colin FitzGerald <cfitzg1983@gmail.com>
2018-04-13 17:03:54 -0500

    Init

---
+++ ErpReferenceLib.cs
@@ -0,0 +1,24 @@
+﻿namespace SSRSCustomAssembly
+{
+    public class ErpReferenceLib
+    {
+        public static string GetImageForCustomerAccount(int customerID)
+        {
+            //this example is static; in real-world implementation the customerID might fetch value from db or API
+            bool isActiveAndValid = CheckCustomerIDIsValidAndActive(customerID);
+            if (isActiveAndValid)
+            {
+                return "https://abccorp.imagesrv?customerID=" + customerID + ".svg";
+            }
+            else
+            {
+                return "https://abccorp.imagesrv?inactive.svg";
+            }
+        }
+
+        public static bool CheckCustomerIDIsValidAndActive(int customerID)
+        {
+            return customerID >= 100 ? true : false;
+        }
+    }
+}
